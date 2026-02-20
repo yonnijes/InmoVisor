dbfd4b87b461e6366ea9a6994a580272eac077e5
Yonni <yonnijes@gmail.com>
2026-02-20 02:10:59 +0000

    Admin: Initial Electron + React structure with simple-git and sharp dependencies

---
+++ admin-desktop/src/main/index.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.VITE_PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+
+let win: BrowserWindow | null
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    width: 1200,
+    height: 800,
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+  } else {
+    win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+    win = null
+  }
+})
+
+app.whenReady().then(createWindow)
